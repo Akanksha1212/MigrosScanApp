6d978a63a7a613285dd46ada48b514d05ddb2f03
Akanksha1212 <singhakanksha221b@gmail.com>
2023-09-16 17:28:16 +0200

    Boilerplate code

---
+++ lib/screens/BarcodeScanner.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BarcodeScanner extends StatefulWidget {
+  const BarcodeScanner({super.key});
+
+  @override
+  State<BarcodeScanner> createState() => _BarcodeScannerState();
+}
+
+class _BarcodeScannerState extends State<BarcodeScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
